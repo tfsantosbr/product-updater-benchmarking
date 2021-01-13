043d3695274c1eb13e6669b90b729a7d616a03dc
Tiago Santos <tfsantosbr@gmail.com>
2021-01-13 11:07:42 -0300

    implementaçào da parte do banco no processo em batch

--- product-updater-batch-solution/src/ProductIteratorApp/Program.cs
+++ product-updater-batch-solution/src/ProductIteratorApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using ProductIteratorApp.Repositories;
 
 namespace ProductIteratorApp
 {
@@ -10,18 +11,23 @@ namespace ProductIteratorApp
         {
             // Ler Arquivo CSV
 
-            var caminhoArquivo = "C:\\Users\\tfsan\\Github\\product-updater-benchmarking\\data\\produtos.CSV";
+            var idLoja = Convert.ToInt16(args[0]);
+            var caminhoArquivo = $"C:/Users/tfsan/Projetos/Sitemercado/@arquivos/produtos-loja-{idLoja}.csv";
             var watch = Stopwatch.StartNew();
-            
-            ProcessaArquivo(caminhoArquivo);
+
+            Console.WriteLine($"Processando Loja: {idLoja}");
+
+            ProcessaArquivo(idLoja, caminhoArquivo);
 
             watch.Stop();
             var elapsedMs = watch.Elapsed;
             Console.WriteLine($"- Tempo de Processamento: {elapsedMs.Hours}:{elapsedMs.Minutes}:{elapsedMs.Seconds}:{elapsedMs.Milliseconds}");
         }
 
-        private static void ProcessaArquivo(string caminhoArquivo)
+        private static void ProcessaArquivo(int idLoja, string caminhoArquivo)
         {
+            var linhasProcessadas = 0;
+            var totalLinhas = 0;
             using var reader = new StreamReader(caminhoArquivo);
 
             reader.ReadLine();
@@ -30,15 +36,46 @@ namespace ProductIteratorApp
             {
                 var linha = reader.ReadLine();
 
-                ProcessarLinha(linha);
+                if (ProcessarLinha(idLoja, linha))
+                {
+                    linhasProcessadas++;
+                }
+
+                totalLinhas++;
             }
+
+            Console.WriteLine("Final do Processamento...");
+            Console.WriteLine($"Total: {totalLinhas}");
+            Console.WriteLine($"Processadas: {linhasProcessadas}");
         }
 
-        private static void ProcessarLinha(string linha)
+        private static bool ProcessarLinha(int idLoja, string linha)
         {
             var valores = linha.Split(';');
 
+            // Valida linha
+
+            if (!ValoresValidos(valores))
+            {
+                return false;
+            }
+
+            // Atualiza registro no banco
+
+            var repository = new ProductRepository();
+            if (!repository.AtualizaProduto(idLoja, valores))
+            {
+                return false;
+            }
+
             Console.WriteLine($"Propriedades {valores.Length} | {linha}");
+
+            return true;
+        }
+
+        private static bool ValoresValidos(string[] valores)
+        {
+            return valores.Length == 7;
         }
     }
 }
---
+++ product-updater-batch-solution/src/ProductIteratorApp/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace ProductIteratorApp.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly string _connectionString = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=SiteMercadoIntegracao;Integrated Security=SSPI";
+
+        public bool AtualizaProduto(int idLoja, string[] valores)
+        {
+            var sql = @"UPDATE [Produtos]
+                           SET [nome] = @nome
+                              ,[sku] = @sku
+                              ,[preco] = @preco
+                              ,[estoque] = @estoque
+                              ,[data_sincronizacao] = @data_sincronizacao
+                         WHERE [id_loja] = @id_loja 
+                           AND [id_produto] = @id_produto";
+
+            using var connection = new SqlConnection(_connectionString);
+
+            var rowsAffected = connection.Execute(sql, new
+            {
+                nome = valores[2],
+                preco = valores[3],
+                estoque = valores[4],
+                data_sincronizacao = DateTime.Now,
+                id_loja = idLoja,
+                sku = valores[1],
+                id_produto = valores[0]
+            });
+
+            return rowsAffected > 0;
+        }
+    }
+}
